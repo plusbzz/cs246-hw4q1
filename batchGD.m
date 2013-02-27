2e6b65d97c84f741f60cc13327be471f25a691e7
Raj Bandyopadhyay <plusbzz@gmail.com>
2013-02-27 15:05:44 -0500

    SGD still doesnt work

--- batchGD.m
+++ batchGD.m
@@ -1,28 +1,56 @@
-function [W,b] = batchGD(X,Y,W,b,C,eta)
+function batchData = batchGD(X,Y)
 %BATCHGD: One step of batch gradient descent
+
+    % Batch gradient descent
     N = size(X,1);
     D = size(X,2);    
     
-    preds = Y.*(X*W+b);
-    
-    % Update W
-    for j = 1:D
-        gradLw = 0;
-        for i = 1:N  % gradient of loss function
-            if preds(i) < 1
-                gradLw = gradLw - Y(i)*X(i,j);
+    C = 100;
+    eta = 0.0000003;
+    epsilon = 0.25;
+    W = zeros(D,1);
+    b = 0;
+
+    lastCost = costFunc(X,Y,W,b,C);
+    k = 0;
+    converged = false;
+    batchData = [k lastCost 0];
+    tic;
+
+    while ~converged
+        k = k+1;
+        preds = Y.*(X*W+b);
+
+        % Update W
+        for j = 1:D
+            gradLw = 0;
+            for i = 1:N  % gradient of loss function
+                if preds(i) < 1
+                    gradLw = gradLw - Y(i)*X(i,j);
+                end
             end
+            W(j) = W(j) - eta*(W(j)+C*gradLw); 
         end
-        W(j) = W(j) - eta*(W(j)+C*gradLw); 
+
+        %Update b
+        gradB = 0;
+        for i = 1:N
+           if preds(i) < 1
+                gradB = gradB - Y(i)*b;
+           end
+        end
+        b = b - eta*C*gradB;
+ 
+        currentCost = costFunc(X,Y,W,b,C);
+        DPerc = (100*abs(currentCost - lastCost))/abs(lastCost);
+        batchData = [batchData; k currentCost toc];   
+        lastCost = currentCost;
+        if DPerc < epsilon
+            converged = true;
+        end
     end
-    
-    %Update b
-    gradB = 0;
-    for i = 1:N
-       if preds(i) < 1
-            gradB = gradB - Y(i)*b;
-       end
-    end
-    b = b - eta*C*gradB;
+
+
+       
 end
 
--- q1.m
+++ q1.m
@@ -8,63 +8,41 @@ Ntrain = size(Xtrain,1);
 Dtrain = size(Xtrain,2);
 
 
-% % Batch gradient descent
+% Batch gradient descent
+batchData = batchGD(Xtrain,Ytrain);
+
+% % Stochastic gradient descent
 % C = 100;
-% eta = 0.0000003;
-% epsilon = 0.25;
+% eta = 0.0001;
+% epsilon = 0.001;
 % W = zeros(Dtrain,1);
 % b = 0;
 % 
 % lastCost = costFunc(Xtrain,Ytrain,W,b,C);
 % k = 0;
 % converged = false;
-% batchData = [k lastCost 0];
+% stochasticData = [k lastCost 0];
+% DCost = 0;
 % tic;
+% idx = randperm(Ntrain);
 % 
-% while ~converged
-%     k = k+1;
-%     [W,b] = batchGD(Xtrain,Ytrain,W,b,C,eta);
-%     currentCost = costFunc(Xtrain,Ytrain,W,b,C)
-%     DPerc = (100*abs(currentCost - lastCost))/abs(lastCost)
-%     batchData = [batchData; k currentCost toc];   
-%     lastCost = currentCost;
-%     if DPerc < epsilon
-%         converged = true;
+% while ~converged    
+%     for i = idx
+%         x = Xtrain(i,:);
+%         y = Ytrain(i);
+%         [W,b] = stochasticGD(x,y,W,b,C,eta);
+%         k = k+1;
+%         currentCost = costFunc(Xtrain,Ytrain,W,b,C);
+%         DPerc = (100*abs(currentCost - lastCost))/abs(lastCost);
+%         DCost = 0.5*(DCost + DPerc);
+%         stochasticData = [stochasticData; k currentCost toc];   
+%         lastCost = currentCost;
+%         if DCost < epsilon
+%             converged = true;
+%             break;
+%         end
 %     end
+%     currentCost 
+%     DCost 
+%     DPerc
 % end
-
-% Stochastic gradient descent
-C = 100;
-eta = 0.0001;
-epsilon = 0.001;
-W = zeros(Dtrain,1);
-b = 0;
-
-lastCost = costFunc(Xtrain,Ytrain,W,b,C);
-k = 0;
-converged = false;
-stochasticData = [k lastCost 0];
-DCost = 0;
-tic;
-idx = randperm(Ntrain);
-
-while ~converged    
-    for i = idx
-        x = Xtrain(i,:);
-        y = Ytrain(i);
-        [W,b] = stochasticGD(x,y,W,b,C,eta);
-        k = k+1;
-        currentCost = costFunc(Xtrain,Ytrain,W,b,C);
-        DPerc = (100*abs(currentCost - lastCost))/abs(lastCost);
-        DCost = 0.5*(DCost + DPerc);
-        stochasticData = [stochasticData; k currentCost toc];   
-        lastCost = currentCost;
-        if DCost < epsilon
-            converged = true;
-            break;
-        end
-    end
-    currentCost 
-    DCost 
-    DPerc
-end
